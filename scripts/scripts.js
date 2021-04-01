d4a018d9b7081361dd26a3ff7dc90a14daed4706
Bryan Xu <xubryan123@gmail.com>
2021-03-31 23:08:32 -0700

    added int check for input and else statement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,3 +15,19 @@ let course3 = {
 
 let courseList = [course1, course2, course3];
 check_code = window.prompt('What course would you like to check for?')
+while(isNaN(check_code) || check_code.length != 4){
+    check_code = window.prompt('What course would you like to check for?')
+}
+for(let course of courseList){
+    if(course.code.includes(check_code)){
+        console.log('Yes I am taking the course: ' + course.code)
+    }
+    else{
+        let course4 = {
+            code: 'ACIT ' + check_code,
+            name: null
+        }
+        courseList.push(course4)
+    }
+}
+
