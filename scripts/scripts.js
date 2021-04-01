277b75b51e64be7596b1a0f234069e0b4e331a81
Bryan Xu <xubryan123@gmail.com>
2021-03-31 22:37:28 -0700

    added objects and array

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+let course1 = {
+    code: 'ACIT 1620',
+    name: 'Web Technologies'
+};
+
+let course2 = {
+    code: 'ACIT 1420',
+    name: 'Introduction to Systems Administration'
+};
+
+let course3 = {
+    code: 'ACIT 1630',
+    name: 'Database System'
+};
+
+let courseList = [course1, course2, course3];
+check_code = window.prompt('What course would you like to check for?')
